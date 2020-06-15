9fe5e6c25a067773facf95fcf91e2fac690dcdc8
Hosam <hd.006@hotmail.com>
2020-06-15 20:25:43 +0200

    Created the menu bar

---
+++ src/typings/general.ts
@@ -0,0 +1,15 @@
+export interface IIcon {
+  width?: number
+  height?: number
+  color?: string
+  className?: string
+}
+
+export interface IColor {
+  color: string
+}
+
+export interface IImage {
+  src: string
+  alt: string
+}
