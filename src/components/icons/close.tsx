76ecfe4dd32c008f061c6db76088ba3edecbd278
Hosam Darwish <hosam.darwish@jungleminds.nl>
2020-07-11 22:42:25 +0200

    Added the close icon

---
+++ src/components/icons/close.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+// Types
+import { IIcon } from "../../typings/general"
+
+// Styling
+import { SVG } from "./favicon"
+
+const CloseSVG = ({ className, color }: IIcon) => (
+  <SVG
+    className={className}
+    fill={color}
+    width="39"
+    height="36"
+    viewBox="0 0 39 36"
+  >
+    <path
+      stroke-width="3"
+      stroke={color}
+      d="M4.061 1.939l33 33M1.939 34.939l33-33"
+    />
+  </SVG>
+)
+
+export default CloseSVG
