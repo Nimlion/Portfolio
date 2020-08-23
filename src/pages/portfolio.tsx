389a8bf564cea4ff8d6b4e9f84a29914db217723
Hosam Darwish <hd.006@hotmail.com>
2020-08-23 13:40:10 +0200

    Added the portfolio page with the projects

---
+++ src/pages/portfolio.tsx
@@ -0,0 +1,46 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+// Components
+import SEO from "../components/seo"
+import Hero from "../components/hero"
+import Layout from "../components/layout"
+import Projects from "../components/projects"
+
+const PortfolioPage = (prismicData: any) => (
+  <Layout>
+    <SEO title="Portfolio" />
+    <Hero title="Portfolio" />
+    <Projects projects={prismicData.data.allPrismicProject.nodes} />
+  </Layout>
+)
+
+export default PortfolioPage
+
+export const query = graphql`
+  {
+    allPrismicProject {
+      nodes {
+        data {
+          project_hero {
+            alt
+            url
+          }
+          project_title {
+            text
+          }
+          carousell {
+            image {
+              alt
+              url
+            }
+          }
+          codeurl
+          designurl
+          domainurl
+          technologies
+        }
+      }
+    }
+  }
+`
